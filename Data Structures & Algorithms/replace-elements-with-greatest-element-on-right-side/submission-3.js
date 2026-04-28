c40c74c65043150387bbc6c121f496cb310acd1e
Pedro Ferreira <id.pedroferreira@outlook.com>
2026-04-28 18:47:29 -0300

    Add: replace-elements-with-greatest-element-on-right-side - submission-3

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let result = [...arr]
+        if(result.length > 1) {
+        let bigIndex = -1
+        let currentIndex = 0
+        while(currentIndex < arr.length){
+            if(currentIndex >= bigIndex){
+                bigIndex = currentIndex+1
+                for(let i = currentIndex + 1; i <= arr.length;i++){
+                    let currentValue = arr[bigIndex] ?? 0
+                    let indexValue = arr[i]
+                    bigIndex = currentValue <= indexValue  ? i : bigIndex
+                }
+            }
+            result[currentIndex] = result[bigIndex]
+            currentIndex++
+        }
+        }
+        result[result.length-1] = -1; 
+        return result
+        
+    }
+}
